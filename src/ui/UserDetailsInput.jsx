34bbe7d5d53c7c79bdc3b2536cc9a7ce0ea5eaa7
Justin Joseph <justin.joseph72@outlook.com>
2018-10-10 16:57:06 +0100

    adding code to update values to state from input fields

--- src/ui/UserDetailsInput.jsx
+++ src/ui/UserDetailsInput.jsx
@@ -4,7 +4,6 @@ class UserDetailsInput extends React.Component{
     return(
       <div>
         <UserDetailsInputTable/>
-        <SubmitPersonDetails/>
       </div>
     );
   }
@@ -15,80 +14,96 @@ class UserDetailsInputTable extends React.Component{
     super(props);
 
     this.state ={
-      givenName: "Given Name",
-      familyName: "Family Name",
-      emailId: "Email",
-      phone: "Phone",
-      rememberMeId: "Remeber Me Id"
+      givenName: "",
+      familyName: "",
+      emailId: "",
+      phone: "",
+      rememberMeId: ""
     }
+    this.updateGivenName = this.updateGivenName.bind(this);
+    this.updateFamilyName = this.updateFamilyName.bind(this);
+    this.updateEmailId = this.updateEmailId.bind(this);
+    this.updatePhone = this.updatePhone.bind(this);
+    this.updateRememberMeId = this.updateRememberMeId.bind(this);
   }
+
+  btnClicked() {
+    var postDataStr = JSON.stringify(this.state);
+    alert("button clicked" + postDataStr);
+  }
+
+  updateGivenName(e){
+    this.setState({givenName: e.target.value});
+  }
+  updateFamilyName(e){
+    this.setState({familyName: e.target.value});
+  }
+  updateEmailId(e){
+    this.setState({emailId: e.target.value});
+  }
+  updatePhone(e){
+    this.setState({phone: e.target.value});
+  }
+  updateRememberMeId(e){
+    this.setState({rememberMeId: e.target.value});
+  }
+
   render(){
     return(
+      <div>
       <div className="form-group">
         <table className="table table-hover">
           <tbody>
           <tr>
             <td >
-              {this.state.givenName}
+              Given Name
             </td>
             <td >
-              <input id ="givenName" className="form-control"/>
-            </td>
-          </tr>
-          <tr>
-            <td >
-              {this.state.familyName}
-            </td>
-            <td >
-              <input id ="familyName" className="form-control"/>
+              <input id ="givenName" className="form-control" value={this.state.givenName} onChange = {this.updateGivenName}/>
             </td>
           </tr>
           <tr>
             <td >
-              {this.state.emailId}
+              Family Name
             </td>
             <td >
-              <input id ="emailId" className="form-control"/>
-            </td>
-          </tr>
-          <tr>
-            <td >
-              {this.state.phone}
-            </td>
-            <td >
-              <input id ="phoneNumber" className="form-control"/>
+              <input id ="familyName" className="form-control" value={this.state.familyName} onChange = {this.updateFamilyName}/>
             </td>
           </tr>
           <tr>
             <td >
-              {this.state.rememberMeId}
+              Email
             </td>
             <td >
-              <input id ="rememberMeId" className="form-control"/>
+              <input id ="emailId" className="form-control" value={this.state.emailId} onChange = {this.updateEmailId}/>
+            </td>
+          </tr>
+          <tr>
+            <td >
+              Phone
+            </td>
+            <td >
+              <input id ="phoneNumber" className="form-control" value={this.state.phone} onChange = {this.updatePhone}/>
+            </td>
+          </tr>
+          <tr>
+            <td >
+              Remember Me Id
+            </td>
+            <td >
+              <input id ="rememberMeId" className="form-control" value={this.state.rememberMeId} onChange = {this.updateRememberMeId}/>
             </td>
           </tr>
           </tbody>
         </table>
       </div>
-
-    );
-  }
-}
-
-class SubmitPersonDetails extends React.Component{
-  btnClicked() {
-    var postData ={
-      "givenName": "sdf"
-    };
-    alert("button clicked" + postData);
-  }
-  render(){
-    return(
       <div>
-        <button onClick={this.btnClicked}>Login</button>
+        <button onClick={this.btnClicked.bind(this)}>Login</button>
+      </div>
       </div>
     );
   }
 }
+
 
 export default UserDetailsInput;
